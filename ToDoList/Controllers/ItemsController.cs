1b47ae0d4c7571b924a901086a0c0b0989f00d66
Jonathan Cheng <jcheng461@gmail.com>
2023-10-08 20:46:43 -0700

    complete refactor to include m2m delete for Tags and ItemTags

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -106,6 +106,15 @@ namespace ToDoList.Controllers
         _db.SaveChanges();
       }
       return RedirectToAction("Details", new { id = item.ItemId });
-    }  
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      ItemTag joinEntry = _db.ItemTags.FirstOrDefault(entry => entry.ItemTagId == joinId);
+      _db.ItemTags.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
