09ac6ed56f004ea2958b2ea52adb7cb3b13a7bcf
Jonathan Cheng <jcheng461@gmail.com>
2023-10-09 16:23:05 -0700

    use migrations to update database; add m2m join entities, read functionality

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
-using MySqlConnector;
 
 namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; set; }
     public int ItemId { get; set; } // uppercase and syntax name must match what these are called in the database. Primary key (id number) follows [ClassName]Id syntax.
-    public Category Category { get; set; }
+    public string Description { get; set; }
     public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<ItemTag> JoinEntities { get; }
   }
 }
