2f30533a46f9c8ae03777acc1530c408dcfbaa7e
Jonathan Cheng <jcheng461@gmail.com>
2023-10-11 22:07:09 -0700

    WIP: add data annotation to customize entity properties

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,17 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
   public class Item
   {
     public int ItemId { get; set; } // uppercase and syntax name must match what these are called in the database. Primary key (id number) follows [ClassName]Id syntax.
+    [Required(ErrorMessage = "Item description required!")]
     public string Description { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
     public int CategoryId { get; set; }
+
     public Category Category { get; set; }
     public List<ItemTag> JoinEntities { get; }
   }
